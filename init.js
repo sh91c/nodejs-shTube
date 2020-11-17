4f1512c24e06417986b337f0e94a85fd8aca4cf5
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-18 01:56:21 +0900

    update

--- init.js
+++ init.js
@@ -8,6 +8,6 @@ import app from './app';
 
 dotenv.config();
 
-app.set('port', process.env.PORT || 9090);
+app.set('port', process.env.PORT || 8080);
 
 app.listen(app.get('port'), () => console.log(`✅ Listening on: http://localhost:${app.get('port')}`));
