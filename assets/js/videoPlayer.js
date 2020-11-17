97954b0d315bcc0a92b56e097e6831608a376cf0
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-18 00:18:44 +0900

    조회수 증가 구현

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -7,6 +7,13 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("jsVolume");
 
+const registerView = () => {
+  const videoId = window.location.href.split('/videos/')[1];
+  fetch(`/api/${videoId}/view`, {
+    method: 'POST',
+  });
+};
+
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -94,7 +101,6 @@ async function setTotalTime() {
 }
 
 function handleEnded() {
-  registerView();
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
@@ -114,6 +120,7 @@ function handleDrag(event) {
 }
 
 function init() {
+  registerView();
   videoPlayer.volume = 0.5;
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
