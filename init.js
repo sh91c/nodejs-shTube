e61a2464c2e84a66d6648b1fa42725405ea8e4ad
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-18 00:21:43 +0900

    Update init.js

--- init.js
+++ init.js
@@ -10,17 +10,4 @@ dotenv.config();
 
 app.set('port', process.env.PORT || 9090);
 
-// // 404 처리
-// app.use((req, res, next)=> {
-//   const error = new Error(`${req.method} ${req.url} Not found.`);
-//   error.status = 404;
-//   next(error);
-// });
-// // error 처리
-// app.use((err, req, res, next) => {
-//   res.locals.message = err.message;
-//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-//   res.render('error');
-// });
-
 app.listen(app.get('port'), () => console.log(`✅ Listening on: http://localhost:${app.get('port')}`));
