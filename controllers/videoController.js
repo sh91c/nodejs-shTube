dab9ca503797ff99cc080fc4946b9cecb41f59e6
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-18 00:21:36 +0900

    버그 fix 및 조회수, 댓글 컨트롤러 구현

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from '../routes';
 import Video from '../models/Video';
+import Comment from '../models/Comment'
 
 export const home = async (req, res) => {
   try {
@@ -53,7 +54,9 @@ export const postUpload = async (req, res) => {
 export const videoDetail = async (req, res) => {
   const { params : { id } } = req;
   try {
-    const video = await Video.findById(id).populate('creator');
+    const video = await Video.findById(id)
+      .populate('creator')
+      .populate('comments');
     res.render('videoDetail', { pageTitle : video.title, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -64,13 +67,15 @@ export const videoDetail = async (req, res) => {
 export const getEditVideo = async (req, res) => {
   const { params : { id } } = req;
   try {
-    const video = await Video.findById(id);
-    if (video.creator !== req.user.id) { // /:id/edit으로 요청시 업로더와 현재 로그인된 유저와 일치하지 않으면 수정하지 못하게..
-      throw Error();
+    const video = await Video.findById(id).populate('creator');
+    // console.log('videoCreator', video.creator, 'userId', req.user.id);
+    if (video.creator.id !== req.user.id) { // /:id/edit으로 요청시 업로더와 현재 로그인된 유저와 일치하지 않으면 수정하지 못하게..
+      throw Error('error');
     } else {
-    res.render('editVideo', { pageTitle : `Edit ${video.title}`, video });
+      res.render('editVideo', { pageTitle : `Edit ${video.title}`, video });
     }
   } catch (error) {
+    console.log('errrrrrror: ', error);
     res.redirect(routes.home);
   }
 };
@@ -91,8 +96,8 @@ export const postEditVideo = async (req, res) => {
 export const deleteVideo = async (req, res) => {
   const { params : { id } } = req;
   try {
-    const video = await Video.findById(id);
-    if (video.creator !== req.user.id) {
+    const video = await Video.findById(id).populate('creator');
+    if (video.creator.id !== req.user.id) {
       throw Error();
     } else {
       await Video.findByIdAndDelete({ _id : id });
@@ -100,5 +105,44 @@ export const deleteVideo = async (req, res) => {
     }
   } catch (error) {
     res.redirect(routes.home);
+  }
+};
+
+
+// Register video view
+export const postRegisterView = async(req, res) => {
+  const { params : { id } } = req;
+  try {
+    const video = await Video.findById(id);
+    video.views += 1;
+    video.save();
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// add comment
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body : { comment },
+    user,
+  } = req;
+  try {
+    const video = await Video.findById(id).populate('comments');
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+      name: user.name,
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
   }
 };
